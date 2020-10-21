c67f10fc3f26874dec3f80c9967bc4c6ce791291
jayasankar123 <jayasankarreddy0@gmail.com>
2020-10-21 21:41:43 +0530

    ButtonClickToRenderPara

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button onClick={() => this.setState({ clicked: true })}>
+          CLICK TO SEE PARA
+        </button>
+        {this.state.clicked
+          ? "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          : ""}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
